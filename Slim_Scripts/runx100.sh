83ebd37a81b7185ffb2f6dab32370b6a61568463
Alexander Curtis <alexander.r.curtis36@gmail.com>
2026-02-26 12:37:40 -0700

    size tests

--- Slim_Scripts/runx100.sh
+++ Slim_Scripts/runx100.sh
@@ -10,7 +10,7 @@ rm -f sexual_fitness_over_time.txt
 rm -f asexual_fitness_over_time.txt
 
 for val in T F; do
-    for i in {1..1}; do
+    for i in {1..100}; do
         echo "Running simulation $i..."
         slim -d asexual="\"$val\"" GrowthFitness.slim
     done
