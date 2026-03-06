e55c0206c2c9708bbee76c5aecb49dda72ad4648
Alexander Curtis <alexander.r.curtis36@gmail.com>
2026-03-06 10:18:33 -0700

    big move rewrites

--- Slim_Scripts/runx100.sh
+++ Slim_Scripts/runx100.sh
@@ -4,42 +4,50 @@ start=$(date +%s)
 
 source venv/bin/activate
 
-epi=T
 
-for mut_profile in 1 2 3; do
-    for preload_location in "mito" "nuc"; do
-        echo "Running simulations with mut_profile=$mut_profile and preload_location=$preload_location"
+for epi in T F; do
+    for mut_profile in 1 2 3; do
+        for preload_location in "mito" "nuc"; do
+            echo "Running simulations with mut_profile=$mut_profile and preload_location=$preload_location"
 
-        rm -f sexual_cycles_to_escape.txt
-        rm -f sexual_final_fitness.txt
-        rm -f sexual_fitness_over_time.txt
+            rm -f sexual_cycles_to_escape.txt
+            rm -f sexual_final_fitness.txt
+            rm -f sexual_fitness_over_time.txt
 
-        rm -f asexual_cycles_to_escape.txt
-        rm -f asexual_final_fitness.txt
-        rm -f asexual_fitness_over_time.txt
+            rm -f asexual_cycles_to_escape.txt
+            rm -f asexual_final_fitness.txt
+            rm -f asexual_fitness_over_time.txt
 
-        plot_dir="automatic_figures"
+            plot_dir="automatic_figures"
 
-        plot_filename="plot_mut${mut_profile}_preload[${preload_location}]_epi${epi}.png"
+            plot_filename="plot_mut${mut_profile}_preload[${preload_location}]_epi${epi}.png"
 
-        for val in T F; do
-            if [ "$val" = "F" ]; then
-                echo "Running sexual simulations..."
-            else
-                echo "Running asexual simulations..."
-            fi 
-            for i in {1..100}; do
-                echo "\n\n\n"
-                echo "Running simulation $i..."
-                slim -d asexual="\"$val\"" -d mut_profile="$mut_profile" -d preload_location="\"$preload_location\"" -d epi="\"$epi\"" GrowthFitness.slim
-                python3 figure_maker.py --output ${plot_dir}/${plot_filename}
+            for val in T F; do
+                if [ "$val" = "F" ]; then
+                    echo "Running sexual simulations..."
+                else
+                    echo "Running asexual simulations..."
+                fi 
+                for i in {1..100}; do
+                    echo
+                    echo
+                    echo
+                    echo "Running simulation $i..."
+                    slim \
+                    -d logging=T \
+                    -d asexual="\"$val\"" \
+                    -d mut_profile="$mut_profile" \
+                    -d preload_location="\"$preload_location\"" \
+                    -d epi="\"$epi\"" \
+                    GrowthFitness.slim
+                    
+                    python3 figure_maker.py --output ${plot_dir}/${plot_filename}
 
+                done
             done
         done
-
     done
 done
-
 end=$(date +%s)
 echo "Elapsed time of simulation: $((end - start)) seconds"
 
