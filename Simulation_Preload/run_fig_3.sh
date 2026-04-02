a04cefbbcd6fff718d80686160f6c14712bff773
xander36 <alexander.r.curtis36@gmail.com>
2026-04-02 13:32:27 -0600

    parameterizations - synonymous mutation fixes - meeting notes - final figure runner script

---
+++ Simulation_Preload/run_fig_3.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+start=$(date +%s)
+
+source venv/bin/activate
+
+tags = (2 20 100 20 20)
+epis = (100 100 100 50 1000)
+
+
+for epi_const in epis; do
+    for num_tags in tags; do
+        run_id="tags${num_tags}_epi${epi_const}"
+
+        echo "Preparing $run_id..."
+
+        plot_dir="automatic_figures/fig_3"
+        plot_filename="plot_${run_id}.png"
+        plot_path="${plot_dir}/${plot_filename}"
+
+        # Skip if figure already exists
+        if [ -f "$plot_path" ]; then
+            echo "Figure $plot_path already exists. Skipping simulations."
+            continue
+        fi
+
+        echo "Running simulations..."
+
+        for val in T F; do
+
+            if [ "$val" = "F" ]; then
+                echo "Running sexual simulations..."
+                prefix="sexual"
+            else
+                echo "Running asexual simulations..."
+                prefix="asexual"
+            fi 
+
+            data_file="${plot_dir}/${prefix}_${run_id}_data.txt"
+
+            rm -f $data_file
+
+            for i in {1..3}; do
+                echo
+                echo "Running simulation $i..."
+
+                slim \
+                -d logging=T \
+                -d asexual="\"$val\"" \
+                -d mut_profile=1 \
+                -d preload_location="mito" \
+                -d epi=T \
+                -d data_file="\"$data_file\"" \
+                -d num_tags="$num_tags" \
+                -d epi_rate="$epi_const" \
+                GrowthFitness.slim
+
+                python3 figure_maker.py \
+                    --sexual_data "${plot_dir}/sexual_${run_id}_data.txt" \
+                    --asexual_data "${plot_dir}/asexual_${run_id}_data.txt" \
+                    --output "$plot_path"
+            done
+        done
+    done
+done
+
+end=$(date +%s)
+echo "Elapsed time of simulation: $((end - start)) seconds"
